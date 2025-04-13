66b77a375d1bbcfca0d7363aaf56d3d92ad4c910
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-04-13 11:14:41 +0300

    reflect objects (hacky)

--- tracer.php
+++ tracer.php
@@ -20,29 +20,49 @@ redefine('Phel\Run\RunFacade*', function(...$args) use ($logfile) {
 	$formatted_time = number_format($end - $begin, 4, '.', '');
 
 	$formatValue = function($arg) {
-        if (is_object($arg)) {
-            $class = get_class($arg);
-            $representation = [];
+		if (is_object($arg)) {
+			$class = get_class($arg);
+			$representation = [];
 
-            // Try common methods for string representation
-            if (method_exists($arg, 'getName')) {
-                $representation['name'] = $arg->getName();
-            }
-            if (method_exists($arg, 'getValue')) {
-                $representation['value'] = $arg->getValue();
-            }
-            if (method_exists($arg, '__toString')) {
-                $representation['string'] = (string)$arg;
-            }
+			// Try common methods for string representation
+			if (method_exists($arg, 'getName')) {
+				$representation['name'] = $arg->getName();
+			}
+			if (method_exists($arg, 'getValue')) {
+				$representation['value'] = $arg->getValue();
+			}
+			if (method_exists($arg, '__toString')) {
+				$representation['string'] = (string)$arg;
+			}
 
-            // Include the class name
-            return [
-                'class' => $class,
-                'data' => !empty($representation) ? $representation : 'object'
-            ];
-        }
-        return $arg;
-    };
+			// Use reflection to get object properties if no common methods found
+			if (empty($representation)) {
+				$reflector = new ReflectionClass($arg);
+				$properties = $reflector->getProperties();
+
+				foreach ($properties as $property) {
+					$property->setAccessible(true);
+					if ($property->isInitialized($arg)) {
+						$value = $property->getValue($arg);
+						// Handle simple scalar values, convert complex values to type info
+						if (is_scalar($value) || is_null($value)) {
+							$representation[$property->getName()] = $value;
+						} else {
+							$representation[$property->getName()] = is_object($value) ?
+                            "object:" . get_class($value) : gettype($value);
+						}
+					}
+				}
+			}
+
+			// Include the class name
+			return [
+				'class' => $class,
+				'data' => !empty($representation) ? $representation : 'object'
+			];
+		}
+		return $arg;
+	};
 
 	$formatted_args = array_map($formatValue, $args);
 	$formatted_result = $formatValue($result);
